10fa6d96be4eb9ab4c24ddf268d902025514206d
한지희 <57996351+g2hhh2ee@users.noreply.github.com>
2022-04-18 23:26:04 +0900

    Update 신규아이디추천.js

--- programmers/신규아이디추천.js
+++ programmers/신규아이디추천.js
@@ -9,4 +9,5 @@ function solution(new_id) {
     // const len = answer.length;
     // return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
     return answer.padEnd(3, answer[answer.length-1]);
+    // return new_id.padEnd(3, new_id.slice(-1));
 }

f124f6cda7ed0578850687afdb5aa454e08c8096
onys <thkim2017@gmail.com>
2022-06-28 16:49:25 +0900

    feat: 🎸 가장 큰 수 예외 케이스 추가

--- programmers/onys/06-5th/maxNum.js
+++ programmers/onys/06-5th/maxNum.js
@@ -1,3 +1,4 @@
 function solution(numbers) {
-  return numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+  const answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+  return answer[0] === '0' ? '0' : answer;
 }
